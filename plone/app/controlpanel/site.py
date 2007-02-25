d5df94c02b26a7b8a205b95f5247eeb15a390758
Christian Scholz <cs@comlounge.net>
2007-02-25 21:47:24 +0000

    added webstats_js to the site settings panel and added tests
    svn path=/plone.app.controlpanel/branches/mrtopf-6158-google-analytics-support/; revision=13180

--- plone/app/controlpanel/site.py
+++ plone/app/controlpanel/site.py
@@ -5,6 +5,8 @@ from zope.interface import implements
 from zope.schema import Bool
 from zope.schema import Text
 from zope.schema import TextLine
+from zope.schema import SourceText
+
 
 from Products.CMFCore.utils import getToolByName
 from Products.CMFDefault.formlib.schema import ProxyFieldProperty
@@ -22,52 +24,58 @@ class ISiteSchema(Interface):
                           default=u'')
 
     site_description = Text(title=_(u'Site description'),
-                           description=_(u"The site description is available "
-                               "in syndicated content and elsewhere. Keep it "
-                               "brief."),
+                           description=_(u"""The site description is available
+                               in syndicated content and elsewhere. Keep it
+                               brief."""),
                            default=u'',
                            required=False)
 
     visible_ids = Bool(title=_(u'Show "Short Name" on content?'),
-                       description=_(u"Display and allow users to edit the "
-                           "'Short name' content identifiers, which form the "
-                           "URL part of a content item's address. Once "
-                           "enabled, users will then be able to enable this "
-                           "option in their preferences."),
+                       description=_(u"""Display and allow users to edit the
+                           "Short name" content identifiers, which form the URL
+                           part of a content item's address. Once enabled,
+                           users will then be able to enable this option in
+                           their preferences."""),
                        default=False)
 
     many_users = Bool(title=_(u'Many users/groups?'),
-                      description=_(u"Determines if your Plone is optimized "
-                          "for small or large sites. In environments with a "
-                          "lot of users and/or groups it can be very slow or "
-                          "impossible to build a list all users or groups. "
-                          "This option tunes the user interface and behaviour "
-                          "of Plone for this case by allowing you to search "
-                          "for users/groups instead of listing all of them."),
+                      description=_(u"""Determines if your Plone is optimized
+                          for small or large sites. In environments with a lot
+                          of users and/or groups it can be very slow or
+                          impossible to build a list all users or groups. This
+                          option tunes the user interface and behaviour of Plone
+                          for this case by allowing you to search for
+                          users/groups instead of listing all of them."""),
                       default=True)
 
     enable_link_integrity_checks = Bool(title=_(u'Enable link integrity checks'),
-                          description=_(u"Determines if the users should get "
-                              "warnings when they delete or move content that "
-                              "is linked from inside the site"),
+                          description=_(u"""Determines if the users should get
+                              warnings when they delete or move content that is
+                              linked from inside the site"""),
                           default=True)
 
     ext_editor = Bool(title=_(u'Enable External Editor feature'),
-                          description=_(u"Determines if the external editor "
-                              "feature is enabled. This feature requires a "
-                              "special client-side application installed. The "
-                              "users also have to enable this in their "
-                              "preferences."),
+                          description=_(u"""Determines if the external editor
+                              feature is enabled. This feature requires a
+                              special client-side application installed. The
+                              users also have to enable this in their
+                              preferences."""),
                           default=False)
 
     enable_sitemap = Bool(title=_(u'Provide sitemap.xml.gz in the portal root'),
-                          description=_(u"A sitemap.xml.gz file might be "
-                              "useful for Google and lists all your content "
-                              "along with modification dates. Please note that "
-                              "generating the sitemap is an expensive "
-                              "operation which can be abused to slow down "
-                              "your site."),
-                          default=False)
+                          description=_(u"""A sitemap.xml.gz file might be
+                              useful for Google and lists all your content
+                              along with modification dates"""),
+                          default=True)
+                          
+    webstats_js = SourceText(title=_(u'JavaScript for web statistics support'),
+                        description=_(u"""For enabling web statistics support for
+                        e.g. Google Analytics. Look on http://plone.org for snippets
+                        which you can paste here. Do not paste Google Analytics code 
+                        here directly."""),
+                        default=u'',
+                        required=False)                         
+                          
 
 
 class SiteControlPanelAdapter(SchemaAdapterBase):
@@ -98,15 +106,27 @@ class SiteControlPanelAdapter(SchemaAdapterBase):
             self.portal.description = value.encode(self.encoding)
         else:
             self.portal.description = ''
+            
+    def get_webstats_js(self):
+        description = getattr(self.context, 'webstats_js', u'')
+        return safe_unicode(description)
+
+    def set_webstats_js(self, value):
+        if value is not None:
+            self.context.webstats_js = value.encode(self.encoding)
+        else:
+            self.context.webstats_js = ''
+            
 
     site_title = property(get_site_title, set_site_title)
     site_description = property(get_site_description, set_site_description)
+    webstats_js = property(get_webstats_js, set_webstats_js)
 
     visible_ids = ProxyFieldProperty(ISiteSchema['visible_ids'])
     many_users = ProxyFieldProperty(ISiteSchema['many_users'])
     enable_link_integrity_checks = ProxyFieldProperty(ISiteSchema['enable_link_integrity_checks'])
     ext_editor = ProxyFieldProperty(ISiteSchema['ext_editor'])
-    enable_sitemap = ProxyFieldProperty(ISiteSchema['enable_sitemap'])
+    enable_sitemap = ProxyFieldProperty(ISiteSchema['enable_sitemap'])    
 
 
 class SiteControlPanel(ControlPanelForm):
